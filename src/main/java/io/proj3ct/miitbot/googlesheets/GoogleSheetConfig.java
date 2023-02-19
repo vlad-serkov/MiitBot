bdd8693be3ccda5fd5fa621b6d4bbed319c1daf5
vlad-serkov <lordr7638@gmail.com>
2023-02-20 00:45:48 +0300

    commit

--- src/main/java/io/proj3ct/miitbot/googlesheets/GoogleSheetConfig.java
+++ src/main/java/io/proj3ct/miitbot/googlesheets/GoogleSheetConfig.java
@@ -86,7 +86,7 @@ public class GoogleSheetConfig {
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8887).build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 }
